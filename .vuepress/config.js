7823782835ab34f83aaf107ae8f1d59997c85722
269479841@qq.com <269479841@qq.com>
2018-08-02 11:14:43 +0800

    init, node-sass

---
+++ .vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	locales: {
+		'/': {
+			lang: 'zh-CN',
+			title: 'Mr.Ye',
+			description: 'Mr.Ye blog'
+		}
+	},
+	themeConfig:{
+		lastUpdated: '最近更新',
+		nav: [
+      { text: '2018', link: '/2018/08-02' },
+    ],
+		sidebar:{
+			'/2018/':['08-02']
+		}
+	}
+}
