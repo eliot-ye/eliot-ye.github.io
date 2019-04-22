7f78379c237aa7f6299d6ea8acdc701cbebc56c0
songxin_ye <13760612708@163.com>
2019-04-22 14:40:16 +0800

    使用空格

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,23 +1,23 @@
 module.exports = {
-	base: '/blog/',
-	locales: {
-		'/': {
-			lang: 'zh-CN',
-			title: 'Mr.Ye',
-			description: 'Mr.Ye blog'
-		}
-	},
-	themeConfig: {
-		lastUpdated: '最近更新',
-		repo: 'https://gitee.com/xspiritx/blog/',
-		repoLabel: '去 gitee 评论',
-		nav: [
-			{text: '2018', link: '/2018/08-02'}, 
-			{text: '2019', link: '/2019/04-09'}, 
-		],
-		sidebar: {
-			'/2018/': ['08-02', '08-03', '08-05', '08-08', '08-21', '08-22', '08-24', '11-30'],
-			'/2019/':['04-09']
-		}
-	}
+  base: '/blog/',
+  locales: {
+    '/': {
+      lang: 'zh-CN',
+      title: 'Mr.Ye',
+      description: 'Mr.Ye blog'
+    }
+  },
+  themeConfig: {
+    lastUpdated: '最近更新',
+    repo: 'https://gitee.com/xspiritx/blog/',
+    repoLabel: '去 gitee 评论',
+    nav: [
+      {text: '2018', link: '/2018/08-02'}, 
+      {text: '2019', link: '/2019/04-09'}, 
+    ],
+    sidebar: {
+      '/2018/': ['08-02', '08-03', '08-05', '08-08', '08-21', '08-22', '08-24', '11-30'],
+      '/2019/':['04-09']
+    }
+  }
 }
